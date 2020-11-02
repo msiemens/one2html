a20d3c75a4af4f269b361b21282a401f6dec2597
Markus Siemens <markus@m-siemens.de>
2020-11-02 16:29:10 +0100

    fix(section): allow multiple pages with same title

--- src/section.rs
+++ src/section.rs
@@ -8,12 +8,14 @@ use std::path::{Path, PathBuf};
 
 pub(crate) struct Renderer {
     pub(crate) files: HashSet<String>,
+    pub(crate) pages: HashSet<String>,
 }
 
 impl Renderer {
     pub fn new() -> Self {
         Renderer {
             files: Default::default(),
+            pages: Default::default(),
         }
     }
 
@@ -38,6 +40,7 @@ impl Renderer {
                 let file_name = (&title[0..(min(title.len(), 250))])
                     .trim()
                     .replace("/", "_");
+                let file_name = self.determine_page_filename(&file_name)?;
 
                 let output_file = section_dir.join(file_name + ".html");
 
@@ -62,5 +65,22 @@ impl Renderer {
         fs::write(toc_file, toc_html)?;
 
         Ok(section_dir)
+    }
+
+    pub(crate) fn determine_page_filename(&mut self, filename: &str) -> Result<String> {
+        let mut i = 0;
+        let mut current_filename = filename.to_string();
+
+        loop {
+            if !self.pages.contains(&current_filename) {
+                self.pages.insert(current_filename.clone());
+
+                return Ok(current_filename);
+            }
+
+            i += 1;
+
+            current_filename = format!("{}_{}", filename, i);
+        }
     }
 }
