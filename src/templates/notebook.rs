06970b71bcf24fc92f516ab36203dbe6312115ea
Márton Gunyhó <marci@gunyho.com>
2025-06-16 11:47:24 +0900

    Remove _bool function, shouldn't be needed with askama 0.14 anymore

--- src/templates/notebook.rs
+++ src/templates/notebook.rs
@@ -8,7 +8,6 @@ use color_eyre::Result;
 struct NotebookTemplate<'a> {
     name: &'a str,
     toc: &'a [Toc],
-    _bool: fn(&bool) -> bool,
 }
 
 pub(crate) enum Toc {
@@ -24,15 +23,11 @@ pub(crate) struct Section {
 }
 
 pub(crate) fn render(name: &str, toc: &[Toc]) -> Result<String> {
-    let template = NotebookTemplate { name, toc, _bool };
+    let template = NotebookTemplate { name, toc };
 
     template
         .render()
         .wrap_err("Failed to render notebook template")
-}
-
-fn _bool(b: &bool) -> bool {
-    *b
 }
 
 mod filters {
